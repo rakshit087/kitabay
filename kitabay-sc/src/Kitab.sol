5428cdc82d898ef8ccc498dbee370fcc273d07b0
Rakshit Arora <rakshit087@gmail.com>
2023-05-15 19:38:19 +0530

    finished Kitab contract and Kitab Tests

--- kitabay-sc/src/Kitab.sol
+++ kitabay-sc/src/Kitab.sol
@@ -8,15 +8,22 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 contract Kitab is ERC721Royalty, Ownable {
     using Counters for Counters.Counter;
 
-    string private _baseTokenURI; 
+    string private _baseTokenURI;
+    uint256 public MAX_SUPPLY = 0;
+    uint256 public INITIAL_PRICE = 0;
+
     Counters.Counter private _tokenIdCounter;
 
     constructor(
         string memory name,
         string memory symbol,
-        string memory baseTokenURI
+        string memory baseTokenURI,
+        uint256 maxSupply,
+        uint256 initialPrice
     ) ERC721(name, symbol) {
         _baseTokenURI = baseTokenURI;
+        MAX_SUPPLY = maxSupply;
+        INITIAL_PRICE = initialPrice;
         _setDefaultRoyalty(address(this), 1000);
     }
 
@@ -24,10 +31,17 @@ contract Kitab is ERC721Royalty, Ownable {
         return _baseTokenURI;
     }
 
-    function safeMint(address to) public onlyOwner {
+    function safeMint(address to) public payable {
+        require(_tokenIdCounter.current() < MAX_SUPPLY, "Max supply reached");
+        require(msg.value >= INITIAL_PRICE, "Insufficient funds");
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
     }
 
+    function withdraw() public onlyOwner {
+        uint256 balance = address(this).balance;
+        (bool success, ) = payable(msg.sender).call{value: balance}("");
+        require(success, "Withdrawal failed");
+    }
 }
--- kitabay-sc/test/Kitab.t.sol
+++ kitabay-sc/test/Kitab.t.sol
@@ -5,18 +5,62 @@ import "forge-std/Test.sol";
 import "../src/Kitab.sol";
 
 contract KitabTest is Test {
-
     Kitab kitab;
-    address addr1 = address(0x1);
-    address addr2 = address(0x2);
-    address addr3 = address(0x3);
+    address public owner;
+    uint256 public maxSupply = 10;
+    uint256 public initialPrice = 1 ether;
+
+    fallback() external payable {}
+    receive() external payable {}
 
     function setUp() public {
-        kitab = new Kitab("The Achemist", "ALC", "https://example.com/");
+        kitab = new Kitab(
+            "Kitab",
+            "KITAB",
+            "https://example.com/",
+            maxSupply,
+            initialPrice
+        );
+        owner = address(this);
     }
 
     function testMint() public {
-        kitab.safeMint(addr1);
-        assertEq(kitab.balanceOf(addr1), 1);
+        address addr1 = address(0x1);
+        uint256 counter = 0;
+        kitab.safeMint{value: initialPrice}(addr1);
+        counter++;
+        assertTrue(kitab.balanceOf(addr1) == 1, "addr1 should own 1 token");
+        assertTrue(address(kitab).balance == initialPrice, "contract should have 1 ether");
+    }
+
+    function testMintMaxSupply() public {
+        address addr1 = address(0x1);
+        uint256 counter = 0;
+        while (counter < maxSupply) {
+            kitab.safeMint{value: initialPrice}(addr1);
+            counter++;
+        }
+        assertTrue(kitab.balanceOf(addr1) == maxSupply, "addr1 should own 10 tokens");
+        assertTrue(address(kitab).balance == maxSupply * initialPrice, "contract should have 10 ether");
+        vm.expectRevert();
+        kitab.safeMint{value: initialPrice}(addr1);
+    }
+
+    function testMintInsufficientFunds() public {
+        address addr1 = address(0x1);
+        vm.expectRevert();
+        kitab.safeMint{value: initialPrice - 1}(addr1);
+    }
+
+    function testWithdraw() public {
+        address addr1 = address(0x1);
+        uint256 counter = 0;
+        while (counter < maxSupply) {
+            kitab.safeMint{value: initialPrice}(addr1);
+            counter++;
+        }
+        assertTrue(address(kitab).balance == maxSupply * initialPrice, "contract should have 10 ether");
+        kitab.withdraw();
+        assertTrue(address(kitab).balance == 0, "contract should have 0 ether");
     }
 }
