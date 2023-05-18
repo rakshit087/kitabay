cbd1db0d877e40c386af4edd91a5086899d0a972
Rakshit Arora <rakshit087@gmail.com>
2023-05-18 13:54:45 +0530

    moved RainbowKit to seperate file

---
+++ kitabay-app/src/contexts/rainbowKit.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { getDefaultWallets, RainbowKitProvider, lightTheme } from '@rainbow-me/rainbowkit';
+import { filecoinHyperspace } from 'wagmi/chains';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import { publicProvider } from 'wagmi/providers/public';
+
+export const RainbowKit: React.FC<React.PropsWithChildren> = ({ children }) => {
+  const { chains, publicClient } = configureChains([filecoinHyperspace], [publicProvider()]);
+
+  const { connectors } = getDefaultWallets({
+    appName: 'My RainbowKit App',
+    projectId: 'YOUR_PROJECT_ID',
+    chains,
+  });
+
+  const wagmiConfig = createConfig({
+    autoConnect: true,
+    connectors,
+    publicClient,
+  });
+
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider
+        chains={chains}
+        theme={lightTheme({
+          accentColor: '#79B3B7',
+          accentColorForeground: 'white',
+          borderRadius: 'large',
+          fontStack: 'system',
+          overlayBlur: 'small',
+        })}
+      >
+        {children}
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
+};
--- kitabay-app/src/pages/_app.tsx
+++ kitabay-app/src/pages/_app.tsx
@@ -1,50 +1,18 @@
-import "@/styles/globals.css";
-import "@rainbow-me/rainbowkit/styles.css";
-import type { AppProps } from "next/app";
-import { AuthProvider } from "@/contexts/authContext";
-import {
-  getDefaultWallets,
-  RainbowKitProvider,
-  lightTheme,
-} from "@rainbow-me/rainbowkit";
-import { filecoinHyperspace } from "wagmi/chains";
-import { configureChains, createConfig, WagmiConfig } from "wagmi";
-import { publicProvider } from "wagmi/providers/public";
+import '@/styles/globals.css';
+import '@rainbow-me/rainbowkit/styles.css';
+import type { AppProps } from 'next/app';
+import { AuthProvider } from '@/contexts/authContext';
+import { RainbowKit } from '@/contexts/rainbowKit';
+import { PrimaryLayout } from '@/layouts/PrimaryLayout';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const { chains, publicClient } = configureChains(
-    [filecoinHyperspace],
-    [publicProvider()]
-  );
-
-  const { connectors } = getDefaultWallets({
-    appName: "My RainbowKit App",
-    projectId: "YOUR_PROJECT_ID",
-    chains,
-  });
-
-  const wagmiConfig = createConfig({
-    autoConnect: true,
-    connectors,
-    publicClient,
-  });
-
   return (
-    <WagmiConfig config={wagmiConfig}>
-      <RainbowKitProvider
-        chains={chains}
-        theme={lightTheme({
-          accentColor: "#79B3B7",
-          accentColorForeground: "white",
-          borderRadius: "large",
-          fontStack: "system",
-          overlayBlur: "small",
-        })}
-      >
-        <AuthProvider>
+    <RainbowKit>
+      <AuthProvider>
+        <PrimaryLayout>
           <Component {...pageProps} />
-        </AuthProvider>
-      </RainbowKitProvider>
-    </WagmiConfig>
+        </PrimaryLayout>
+      </AuthProvider>
+    </RainbowKit>
   );
 }
