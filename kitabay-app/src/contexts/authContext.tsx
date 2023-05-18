e49c7b19e9e6b4f2e1fd5323b4366451bf6f306e
Rakshit Arora <rakshit087@gmail.com>
2023-05-19 01:23:36 +0530

    show onboarding card if user is new

--- kitabay-app/src/contexts/authContext.tsx
+++ kitabay-app/src/contexts/authContext.tsx
@@ -1,6 +1,8 @@
-import React, { useEffect } from "react";
-import { createContext, useState } from "react";
-import { useAccount } from "wagmi";
+import React, { useEffect } from 'react';
+import { createContext, useState } from 'react';
+import { useAccount } from 'wagmi';
+import { db } from '@/config/poybase.config';
+import { OnboardingCard } from '@/components/OnboardingCard';
 
 interface UserI {
   address: string;
@@ -18,17 +20,35 @@ export const AuthContext = createContext<{
   authenticated: false,
 });
 
-export const AuthProvider: React.FC<React.PropsWithChildren> = ({
-  children,
-}) => {
+export const AuthProvider: React.FC<React.PropsWithChildren> = ({ children }) => {
   const [user, setUser] = useState<UserI | null>(null);
   const [authenticated, setAuthenticated] = useState<boolean>(false);
   const { address, isConnected } = useAccount();
+  const [showOnboarding, setShowOnboarding] = useState<boolean>(false);
 
-  useEffect(() => {}, [address, isConnected]);
+  useEffect(() => {
+    (async () => {
+      if (isConnected && address) {
+        try {
+          const user = await db
+            .collection('User')
+            .record(address as string)
+            .get();
+        } catch (e: any) {
+          if (e.message === 'record/not-found error') {
+            setShowOnboarding(true);
+          }
+        }
+      }
+    })();
+  }, [address, isConnected]);
 
   return (
     <AuthContext.Provider value={{ user, setUser, authenticated }}>
+      <OnboardingCard
+        isOpen={showOnboarding}
+        setIsOpen={setShowOnboarding}
+      />
       {children}
     </AuthContext.Provider>
   );
