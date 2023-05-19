d88ad5d573650ec32801b8168f6e5b19fa1115a9
Rakshit Arora <rakshit087@gmail.com>
2023-05-19 11:07:24 +0530

    made the default outline of Input as Teal

--- kitabay-app/src/styles/theme.ts
+++ kitabay-app/src/styles/theme.ts
@@ -1,10 +1,17 @@
-import { Montserrat } from "next/font/google";
-import { extendTheme } from "@chakra-ui/react"
-
+import { Montserrat } from 'next/font/google';
+import { extendTheme } from '@chakra-ui/react';
+import { withDefaultColorScheme } from '@chakra-ui/react';
 export const montserrat = Montserrat({ subsets: ['latin'] });
 
-export const theme = extendTheme({
-    colors: {
-        background: '#FDFBF3',
+export const theme = extendTheme(withDefaultColorScheme({ colorScheme: 'teal' }), {
+  colors: {
+    background: '#FDFBF3',
+  },
+ components: {
+    Input: {
+        defaultProps: {
+            focusBorderColor: 'teal.500',
+        }
     }
-});
+ }
+},);
