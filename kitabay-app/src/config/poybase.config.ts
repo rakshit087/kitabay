4f9813fea5eab1ddd05461c0cf134f01c2ca3ee0
Rakshit Arora <rakshit087@gmail.com>
2023-05-19 18:03:32 +0530

    added signer to Polybase

--- kitabay-app/src/config/poybase.config.ts
+++ kitabay-app/src/config/poybase.config.ts
@@ -1,6 +1,14 @@
 import { Polybase } from '@polybase/client';
+import { signMessage } from '@wagmi/core';
 
-export const db = new Polybase({
+const db = new Polybase({
   defaultNamespace:
     'pk/0xbaf3185b0b0bd7be972f904b55cc89c1df80057d2ea612a05fa5cbfe3fbad2ac5053724bada19e94bd8848676ad719471100ed8a23371d2c55061baca62b34d0/kitabay',
 });
+
+db.signer(async (data: string) => {
+  const sig = await signMessage({ message: data });
+  return { h: 'eth-personal-sign', sig };
+});
+
+export { db };
