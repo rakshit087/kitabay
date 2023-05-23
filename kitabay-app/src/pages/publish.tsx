4c0cd7c337c43779241e8a9b54b82d1d4d823cbf
Rakshit Arora <rakshit087@gmail.com>
2023-05-23 10:54:19 +0530

    added validation, handle etc

--- kitabay-app/src/pages/publish.tsx
+++ kitabay-app/src/pages/publish.tsx
@@ -1,6 +1,7 @@
-import { Box, VStack } from '@chakra-ui/react';
-import { Montserrat } from 'next/font/google';
 import { useContext } from 'react';
+import { Formik } from 'formik';
+import { Box } from '@chakra-ui/react';
+import { KitabFormSchema } from '@/modules/Publish/validation/kitabFormSchema';
 import { AuthContext } from '@/contexts/authContext';
 import { Introduction } from '@/modules/Publish/components/Introduction';
 import { Title } from '@/modules/Publish/components/Title';
@@ -8,53 +9,47 @@ import { Description } from '@/modules/Publish/components/Description';
 import { Tokenomics } from '@/modules/Publish/components/Tokenomics';
 import { BookCover } from '@/modules/Publish/components/BookCover';
 import { BookContent } from '@/modules/Publish/components/BookContent';
-import { Formik } from 'formik';
-
-const montserrat = Montserrat({ subsets: ['latin'] });
 
 const Publish = () => {
   const { user } = useContext(AuthContext);
   return (
-    <Formik
-      initialValues={{
-        title: '',
-        author: user?.name || '',
-        description: '',
-        copies: 0,
-        price: 0,
-        bookCover: '',
-        bookContent: '',
-      }}
-      onSubmit={(values) => {
-        console.log(values);
-      }}
+    <Box
+      w={'full'}
+      h={'100vh'}
+      overflowY={'scroll'}
+      scrollSnapType={'y mandatory'}
+      scrollBehavior={'smooth'}
     >
-      {({ handleSubmit, errors, touched, values }) => (
-        <Box
-          w={'full'}
-          h={'100vh'}
-          overflowY={'scroll'}
-          scrollSnapType={'y mandatory'}
-          scrollBehavior={'smooth'}
-        >
-          <Introduction />
-          <Title
-            errors={errors}
-            touched={touched}
-          />
-          <Description
-            errors={errors}
-            touched={touched}
-          />
-          <Tokenomics 
-            errors={errors}
-            touched={touched}
-          />
-          <BookCover />
-          <BookContent />
-        </Box>
-      )}
-    </Formik>
+      <Formik
+        initialValues={{
+          title: '',
+          author: user?.name || '',
+          description: '',
+          copies: 0,
+          price: 0,
+          bookCover: '',
+        }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+        validationSchema={KitabFormSchema}
+      >
+        {({ handleSubmit, errors, setFieldValue }) => (
+          <>
+            <Introduction />
+            <Title />
+            <Description />
+            <Tokenomics />
+            <BookCover
+              handleSubmit={handleSubmit}
+              setFieldValue={setFieldValue}
+              errors={errors}
+            />
+          </>
+        )}
+      </Formik>
+      <BookContent />
+    </Box>
   );
 };
 
