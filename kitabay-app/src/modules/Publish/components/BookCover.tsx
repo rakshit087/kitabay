b3f043cd41008710d64a02bc12d04265e17c7417
Rakshit Arora <rakshit087@gmail.com>
2023-05-23 10:53:46 +0530

    added file upload and error handeling

--- kitabay-app/src/modules/Publish/components/BookCover.tsx
+++ kitabay-app/src/modules/Publish/components/BookCover.tsx
@@ -1,13 +1,20 @@
 import { Box, Button, Flex, Text } from '@chakra-ui/react';
-import Link from 'next/link';
+import lighthouse from '@lighthouse-web3/sdk';
+import { useRouter } from 'next/router';
+import { useShowErrorToast } from '@/hooks/showErrorToast';
 import { useState } from 'react';
-import { FilePond } from 'react-filepond';
 
-export const BookCover = () => {
-  const [cover, setCover] = useState([]);
+export const BookCover = (props: {
+  setFieldValue: (field: string, value: any) => void;
+  handleSubmit: () => void;
+  errors: any;
+}) => {
+  const router = useRouter();
+  const { setErrorMessage } = useShowErrorToast();
+  const [uploading, setUploading] = useState<boolean>(false);
   return (
     <Flex
-      height={'100vh'}
+      height={'100%'}
       flexDirection={'column'}
       position={'relative'}
       scrollSnapAlign={'start'}
@@ -31,22 +38,47 @@ export const BookCover = () => {
         This cover will also be a part of NFT's metadata.
       </Text>
       <Box my={12}>
-        <FilePond
-          files={cover}
-          onupdatefiles={setCover as any}
-          allowMultiple={false}
-          labelIdle='Drag & Drop your cover or <span class="filepond--label-action">Browse</span>'
-          acceptedFileTypes={['image/*']}
+        <input
+          type="file"
+          accept="image/*"
+          onChange={async (e) => {
+            setUploading(true);
+            const cidData: any = await lighthouse.upload(e, process.env.NEXT_PUBLIC_LH_ENDPOINT || '');
+            console.log(cidData);
+            props.setFieldValue('bookCover', cidData.data.Hash);
+            setUploading(false);
+          }}
         />
       </Box>
-      <Link href={'#content'}>
-        <Button
-          colorScheme={'teal'}
-          size={'lg'}
-        >
-          Continue
-        </Button>
-      </Link>
+      <Button
+        colorScheme={'teal'}
+        size={'lg'}
+        onClick={() => {
+          if (props.errors.title) {
+            setErrorMessage(props.errors.title);
+            router.push('#title');
+          } else if (props.errors.description) {
+            setErrorMessage(props.errors.description);
+            router.push('#description');
+          } else if (props.errors.copies) {
+            setErrorMessage(props.errors.copies);
+            router.push('#tokenomics');
+          } else if (props.errors.price) {
+            setErrorMessage(props.errors.price);
+            router.push('#tokenomics');
+          } else if (props.errors.bookCover) {
+            setErrorMessage(props.errors.bookCover);
+            router.push('#cover');
+          } else {
+            props.handleSubmit();
+          }
+        }}
+        isLoading={uploading}
+        isDisabled={uploading}
+        w={'12rem'}
+      >
+        Make your Kitab
+      </Button>
     </Flex>
   );
 };
